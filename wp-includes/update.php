7f04a381c11733f9bc2863cdfc900b19bc83db2e
Chang Ying Qiu <root@cyqsd.cn>
2015-12-13 11:00:36 +0800

    屏蔽WordPress权限检查和升级

--- wp-includes/update.php
+++ wp-includes/update.php
@@ -703,6 +703,15 @@ if ( ( ! is_main_site() && ! is_network_admin() ) || ( defined( 'DOING_AJAX' ) &
 //add_action( 'wp_maybe_auto_update', 'wp_maybe_auto_update' );
 
 //add_action( 'init', 'wp_schedule_update_checks' );
- add_action( 'load-update-core.php', 'wp_version_check' );
- add_action( 'load-update-core.php', 'wp_update_themes' );
- add_action( 'load-update-core.php', 'wp_update_plugins' );
+
+add_filter('pre_site_transient_update_core',    create_function('$a', "return null;")); // 关闭核心提示
+
+add_filter('pre_site_transient_update_plugins', create_function('$a', "return null;")); // 关闭插件提示
+
+add_filter('pre_site_transient_update_themes',  create_function('$a', "return null;")); // 关闭主题提示
+
+remove_action('admin_init', '_maybe_update_core');    // 禁止 WordPress 检查更新
+
+remove_action('admin_init', '_maybe_update_plugins'); // 禁止 WordPress 更新插件
+
+remove_action('admin_init', '_maybe_update_themes');  // 禁止 WordPress 更新主题
